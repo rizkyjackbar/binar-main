4408715e16dd92b8221e5f70b29193231fe22382
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-08-31 06:24:00 +0700

    pertemeuan 30Aug23

--- latihanJS/script.js
+++ latihanJS/script.js
@@ -1,20 +1,20 @@
-console.log("Hello World, from eksternal JS");
+// console.log("Hello World, from eksternal JS");
 
-// scope
-// Global scope
+// // scope
+// // Global scope
 
-// local scope
-function test() {
-  var variableInTest = "Good Night!";
-  console.log(variableInTest);
-}
+// // local scope
+// function test() {
+//   var variableInTest = "Good Night!";
+//   console.log(variableInTest);
+// }
 
 
-// let
-// let variableName
+// // let
+// // let variableName
 
-// reassigned
-let testA = 'testA';
-console.log (testA);
-// let testA = 'New testA';
+// // reassigned
+// let testA = 'testA';
 // console.log (testA);
+// // let testA = 'New testA';
+// // console.log (testA);
