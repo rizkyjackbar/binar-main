adf1a3d9091db4f9870deb2705a1764c1adf5b42
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-09-06 06:30:08 +0700

    mengedit isi sortCarByYearDecendingly

---
+++ challenge2/scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,28 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  function sortCarDescendingly(a, b) {
+    return b.year - a.year;
+  }
+
+  // Implementasi algoritma pengurutan
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = 0; j < result.length - i - 1; j++) {
+      if (sortCarDescendingly(result[j], result[j + 1]) > 0) {
+        // Tukar elemen jika perlu
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
