fd81cb257ae12eed3d18492a052aaa38ff66b46b
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-09-13 21:21:18 +0700

    membuat mengguanakan Express

---
+++ projek-node/index-with-express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (req, res)=>{
+    res.status(200).send('Hello World!')
+});
+
+app.listen(PORT, ()=>{
+    console.log("Server sudah berjalan, silahkan buka http://0.0.0.0:%d", PORT)
+})
