6f69e6e94a1d3f1ecb16548b600c9770a2bea3e7
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-09-15 20:19:31 +0700

    melanjutkan c3

--- challange3/index.js
+++ challange3/index.js
@@ -1,40 +1,40 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000; // Anda bisa menggunakan port lain jika diperlukan
 
 // Data array mobil (contoh data)
 const cars = [
-  { id: 1, brand: "Toyota", model: "Camry" },
-  { id: 2, brand: "Honda", model: "Civic" },
+  { id: 1, brand: 'Toyota', model: 'Camry' },
+  { id: 2, brand: 'Honda', model: 'Civic' },
   // Tambahkan data mobil lainnya sesuai kebutuhan
 ];
 
 app.use(express.json());
 
 // Route untuk request / (ping)
-app.get("/", (req, res) => {
-  res.json({ message: "Ping successfully" });
+app.get('/', (req, res) => {
+  res.json({ message: 'Ping successfully' });
 });
 
 // Route untuk request list cars
-app.get("/list", (req, res) => {
+app.get('/list', (req, res) => {
   res.json(cars);
 });
 
 // Route untuk request detail cars berdasarkan ID
-app.get("/detail/:id", (req, res) => {
+app.get('/detail/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const car = cars.find((c) => c.id === id);
 
   if (!car) {
-    return res.status(404).json({ message: "Car not found" });
+    return res.status(404).json({ message: 'Car not found' });
   }
 
   res.json(car);
 });
 
 // Route untuk request create cars
-app.post("/create", (req, res) => {
+app.post('/create', (req, res) => {
   const newCar = req.body;
   newCar.id = cars.length + 1; // ID baru adalah panjang array + 1
   cars.push(newCar);
@@ -42,13 +42,13 @@ app.post("/create", (req, res) => {
 });
 
 // Route untuk request update cars berdasarkan ID
-app.put("/update/:id", (req, res) => {
+app.put('/update/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const updatedCar = req.body;
   const index = cars.findIndex((c) => c.id === id);
 
   if (index === -1) {
-    return res.status(404).json({ message: "Car not found" });
+    return res.status(404).json({ message: 'Car not found' });
   }
 
   cars[index] = { ...cars[index], ...updatedCar };
@@ -56,12 +56,12 @@ app.put("/update/:id", (req, res) => {
 });
 
 // Route untuk request delete cars berdasarkan ID
-app.delete("/delete/:id", (req, res) => {
+app.delete('/delete/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const index = cars.findIndex((c) => c.id === id);
 
   if (index === -1) {
-    return res.status(404).json({ message: "Car not found" });
+    return res.status(404).json({ message: 'Car not found' });
   }
 
   const deletedCar = cars.splice(index, 1);
