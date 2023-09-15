e77638ca3b681d460e5cb560a2200f73e77fcc3b
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-09-15 20:16:04 +0700

    membuat express

--- projek-node/index-with-express.js
+++ projek-node/index-with-express.js
@@ -6,12 +6,27 @@
 const http = require('http');
 const { PORT = 8000 } = process.env; // Ambil port dari environment variable
 
+const fs = require('fs');
+const path = require('path');
+const PUBLIC_DIRECTORY = path.join(__dirname, 'public'); 
+
 // Request handler
 // Fungsi yang berjalan ketika ada request yang masuk.
 function onRequest(req, res) {
-  // Memberi status 200
-  res.writeHead(200);
-  res.end("Halo dari server!");
+  // Membuat path ke html file yang ada di directory public
+  const htmlFile = path.join(PUBLIC_DIRECTORY, 'index.html');
+
+  // Membaca file HTML pada path htmlFile
+  const html = fs.readFileSync(htmlFile, 'utf-8')
+
+  // Set header agar client tau kalo response kita itu berupa HTML
+  res.setHeader('Content-Type', 'text/html')
+
+  // Set response status, agar client kita tau kalo requestnya berhasil
+  res.writeHead(200)
+
+  // Set response body, agar dapat dibaca oleh client
+  res.end(html)
 }
 
 const server = http.createServer(onRequest);
