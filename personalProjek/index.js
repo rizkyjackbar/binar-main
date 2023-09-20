988bb705fa0ce7b0f49df54b60a9eb12abcf940e
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-09-20 19:01:00 +0700

    edit nama variable

--- personalProjek/index.js
+++ personalProjek/index.js
@@ -7,60 +7,66 @@ const app = express();
 // Middleware untuk mengizinkan aplikasi menerima JSON
 app.use(express.json());
 
+// Anda bisa menggunakan port lain jika diperlukan
 const PORT = 9000;
 
 // Data sementara untuk menyimpan informasi
 let data = [
-  { id: 1, name: 'Item 1' },
-  { id: 2, name: 'Item 2' },
-  { id: 3, name: 'Item 3' },
+  { id: 1, name: 'Car 1' },
+  { id: 2, name: 'Car 2' },
+  { id: 3, name: 'Car 3' },
 ];
 
-// Endpoint untuk mendapatkan semua item
-app.get('/items', (req, res) => {
+// Route untuk request ping
+app.get('/', (req, res) => {
+  res.json({ message: 'Ping successfully' });
+});
+
+// Endpoint untuk mendapatkan semua car
+app.get('/cars', (req, res) => {
   res.json(data);
 });
 
-// Endpoint untuk mendapatkan item berdasarkan ID
-app.get('/items/:id', (req, res) => {
+// Endpoint untuk mendapatkan car berdasarkan ID
+app.get('/cars/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  const item = data.find(item => item.id === id);
+  const car = data.find(item => item.id === id);
 
-  if (!item) {
-    return res.status(404).json({ error: 'Item not found' });
+  if (!car) {
+    return res.status(404).json({ error: 'Car not found' });
   }
 
-  res.json(item);
+  res.json(car);
 });
 
-// Endpoint untuk menambahkan item baru
-app.post('/items', (req, res) => {
-  const newItem = req.body;
-  data.push(newItem);
-  res.status(201).json(newItem);
+// Endpoint untuk menambahkan car baru
+app.post('/cars', (req, res) => {
+  const newCar = req.body;
+  data.push(newCar);
+  res.status(201).json(newCar);
 });
 
-// Endpoint untuk mengubah item berdasarkan ID
-app.put('/items/:id', (req, res) => {
+// Endpoint untuk mengubah car berdasarkan ID
+app.put('/cars/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  const updatedItem = req.body;
+  const updatedCar = req.body;
   const index = data.findIndex(item => item.id === id);
 
   if (index === -1) {
-    return res.status(404).json({ error: 'Item not found' });
+    return res.status(404).json({ error: 'Car not found' });
   }
 
-  data[index] = updatedItem;
-  res.json(updatedItem);
+  data[index] = updatedCar;
+  res.json(updatedCar);
 });
 
-// Endpoint untuk menghapus item berdasarkan ID
-app.delete('/items/:id', (req, res) => {
+// Endpoint untuk menghapus car berdasarkan ID
+app.delete('/cars/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const index = data.findIndex(item => item.id === id);
 
   if (index === -1) {
-    return res.status(404).json({ error: 'Item not found' });
+    return res.status(404).json({ error: 'Car not found' });
   }
 
   data.splice(index, 1);
