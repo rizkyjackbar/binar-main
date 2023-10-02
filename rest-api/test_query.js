22ff4f4adef80b0fa8521cfdcb0250a2cfc503c7
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-02 22:13:19 +0700

    making test query

---
+++ rest-api/test_query.js
@@ -0,0 +1,12 @@
+const {Feeds} = require ('./models');
+const { v4: uuidv4 } = require("uuid");
+// const getAllFeeds = async()=>{
+//     const data = await Feeds.findAll();
+//     console.log(data);
+// }
+
+// getAllFeeds();
+
+// Feeds.findAll().then((data)=>{console.log(data)}).catch(err)=>{console.log(err)};
+
+// INSERT
