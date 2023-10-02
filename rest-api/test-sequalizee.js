b5e40e37eae7bd4a1666774f3d56dfbd0ca1595b
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-02 20:04:47 +0700

    menginstall sequelize-cli dan membuat db baru

--- rest-api/test-sequalizee.js
+++ rest-api/test-sequalizee.js
@@ -1,8 +1,13 @@
-const { Sequelize } = require('sequelize');
-
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('postgres', 'postgres', 'prominent2020', {
   host: 'localhost',
-  dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  dialect: 'postgres'
 });
 
+const test = async()=>{
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
