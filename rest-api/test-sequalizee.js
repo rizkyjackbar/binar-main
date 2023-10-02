d6f0d3e7d6ba64ea3f9be2c3705f0474b62667e8
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-02 18:52:42 +0700

    buat tampilan ch4

---
+++ rest-api/test-sequalizee.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
