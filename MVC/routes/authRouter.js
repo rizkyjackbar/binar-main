b0b0e63c08afe6fa309333e765a1322fce276d2e
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-16 13:48:46 +0700

    delete file

--- MVC/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const Auth = require("../controller/authController");
-
-router.post("/register", Auth.register);
-router.post("/login", Auth.login);
-
-module.exports = router;
