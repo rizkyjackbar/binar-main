2988527485ff2dba996924274d72ade58aa28534
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-16 22:28:08 +0700

    add auth file in middleware

--- rest-apis/app/middleware/auth.js
+++ rest-apis/app/middleware/auth.js
@@ -1,3 +1,15 @@
 exports.authorize = (req, res, next) => {
-    
+    exports.authorize = async (req, res, next) => {
+        try {
+            const bearerToken = req.headers.authorization;
+            const user = await authService.authorize(bearerToken);
+            req.user = user;
+            next();
+        } catch (err) {
+            res.status(err.statusCode || 500).json({
+                status: "FAIL",
+                message: err.message,
+            })
+        }
+    }
 }
