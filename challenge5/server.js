d9ef6e5a275d71d98b6a6face7bd622751ae3474
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-19 19:54:33 +0700

    inisialialisasi server

---
+++ challenge5/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+const secureRoutes = require('./routes/secureRoutes');
+
+app.use(express.json());
+
+// Menggunakan rute user
+app.use('/api', userRoutes);
+
+// Menggunakan rute yang memerlukan autentikasi
+app.use('/api/secure', secureRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
