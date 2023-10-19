508743c58188d1ae2540137f8bda05db18cd490a
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-19 19:52:15 +0700

    make middleware & authenticate

---
+++ challenge5/authMiddleware.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Access denied' });
+
+  jwt.verify(token, 'secret_key', (err, user) => {
+    if (err) return res.status(403).json({ error: 'Invalid token' });
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { authenticateToken };
