6ab53c48da6c5be7a980e3c2968726e7323dc9b8
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-25 15:07:13 +0700

    add WhyUs

---
+++ react-app/my-app/src/components/whyUs/WhyUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function WhyUs() {
+  return (
+    <section id="whyUs">
+      {/* Isi kode Why Us yang sudah Anda berikan */}
+    </section>
+  );
+}
+
+export default WhyUs;
