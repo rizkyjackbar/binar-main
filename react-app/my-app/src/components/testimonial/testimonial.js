7df4d8d0d18796c7bc01381ced426fd2163cffda
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-25 15:07:02 +0700

    add testimonial

---
+++ react-app/my-app/src/components/testimonial/testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Testimonial() {
+  return (
+    <section id="testimonial">
+      {/* Isi kode Testimonial yang sudah Anda berikan */}
+    </section>
+  );
+}
+
+export default Testimonial;
