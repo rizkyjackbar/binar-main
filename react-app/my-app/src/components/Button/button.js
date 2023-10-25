883d30c788fa7802b71571854564706275710f64
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-25 21:40:29 +0700

    correct code

--- react-app/my-app/src/components/Button/button.js
+++ react-app/my-app/src/components/Button/button.js
@@ -1,16 +1,21 @@
-import React, { Fragment } from "react";
+import React from "react";
 
-const bgColor ={
-    primary : 'blue',
-    error : 'red',
-    secondary : 'green',
-    info : 'yellow'
-}
+const bgColor = {
+  primary: "blue",
+  error: "red",
+  secondary: "green",
+  info: "yellow",
+};
 
-const Button = ({text, onclick, variant}) => {
-    return (
-        <button style={{backgroundColor: bgColor[variant], color: 'white'}} onclick={onclick}>{text}</button>
-    )
-}
+const Button = ({ text, onClick, variant }) => {
+  return (
+    <button
+      style={{ backgroundColor: bgColor[variant], color: "white" }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
