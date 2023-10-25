5679f18b72120677e06056b203a990e71bebcc35
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-25 14:43:40 +0700

    konfigurasi app

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,39 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from '../src/components/Button/Button'
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import './Dashboard.css';
+import Navbar from './components/navbar/navbar';
+import OurServices from './components/ourServices/OurServices';
+import Layanan from './components/layanan/layanan';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button
-        text={'View'}
-        variant='primary'
-        onClick={()=>{console.log('View')}}
-        />
-        <Button
-        text={'View'}
-        variant='primary'
-        onClick={()=>{console.log('Delete')}}
-        />
-        <Button
-        text={'View'}
-        variant='primary'
-        onClick={()=>{console.log('Submit')}}
-        />
-      </header>
+      <Navbar />
+      <OurServices />
+      <Layanan />
+      {/* <WhyUs />
+      <Testimonial />
+      <FAQ />
+      <Footer /> */}
     </div>
   );
 }
