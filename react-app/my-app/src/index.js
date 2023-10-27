23900c0407ce01d29820a4a5544491fc4e17b766
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-27 20:30:54 +0700

    make a nested

--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -13,11 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<Home />}></Route>
+      <Route path="/">
+      <Route index element={<Home />}></Route>
       <Route path="/cars" element={<Car />}></Route>
       <Route path="/register" element={<Register />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="*" element={<NotFound />}></Route>
+      </Route>
     </Routes>
   </BrowserRouter>,
 );
