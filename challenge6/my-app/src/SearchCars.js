af1ddccd89fefc1021fbdf2f949e9ad45d106f9e
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-10-28 18:58:10 +0700

    add cari mobil

---
+++ challenge6/my-app/src/SearchCars.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+// import Button from "./components/Button/button";
+// import Button from "@mui/material/Button";
+
+function SearchCars() {
+  //   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Hello");
+  }, []);
+
+  return (
+    <div className="car">
+      <header className="App-header">
+        <h1>This Is Page For Searching Cars</h1>
+      </header>
+    </div>
+  );
+}
+
+export default SearchCars;
--- challenge6/my-app/src/index.js
+++ challenge6/my-app/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import NotFound from "./404";
+import SearchCars from "./SearchCars";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/homepage" element={<App />}></Route>
+      <Route path="/cariMobil" element={<SearchCars />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   </BrowserRouter>
