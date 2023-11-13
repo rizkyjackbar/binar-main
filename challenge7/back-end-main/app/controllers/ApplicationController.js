3b49d33fc6ca32dddba40146f521ef8155692688
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-11-13 23:01:04 +0700

    fixing code for eslint

--- challenge7/back-end-main/app/controllers/ApplicationController.js
+++ challenge7/back-end-main/app/controllers/ApplicationController.js
@@ -1,21 +1,21 @@
-const { NotFoundError } = require("../errors");
+const { NotFoundError } = require('../errors')
 
 class ApplicationController {
   handleGetRoot = (req, res) => {
     res.status(200).json({
-      status: "OK",
-      message: "BCR API is up and running!",
-    });
+      status: 'OK',
+      message: 'BCR API is up and running!'
+    })
   }
 
   handleNotFound = (req, res) => {
-    const err = new NotFoundError(req.method, req.url);
+    const err = new NotFoundError(req.method, req.url)
 
     res.status(404).json({
       error: {
         name: err.name,
         message: err.message,
-        details: err.details,
+        details: err.details
       }
     })
   }
@@ -25,27 +25,27 @@ class ApplicationController {
       error: {
         name: err.name,
         message: err.message,
-        details: err.details || null,
+        details: err.details || null
       }
     })
-  } 
-
-  getOffsetFromRequest(req) {
-    const { page = 1, pageSize = 10 } = req.query;
-    const offset = (page - 1) * pageSize; 
-    return offset;
   }
 
-  buildPaginationObject(req, count) {
-    const { page = 1, pageSize = 10 } = req.query;
-    const pageCount = Math.ceil(count / pageSize);
+  getOffsetFromRequest (req) {
+    const { page = 1, pageSize = 10 } = req.query
+    const offset = (page - 1) * pageSize
+    return offset
+  }
+
+  buildPaginationObject (req, count) {
+    const { page = 1, pageSize = 10 } = req.query
+    const pageCount = Math.ceil(count / pageSize)
     return {
       page,
       pageCount,
       pageSize,
-      count,
+      count
     }
   }
 }
 
-module.exports = ApplicationController;
+module.exports = ApplicationController
